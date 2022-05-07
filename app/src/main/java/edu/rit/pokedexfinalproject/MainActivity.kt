6d81e777666e15d48c60a385676cfbef64d056e4
Josipa Puljko <jxp5761@rit.edu>
2022-05-07 20:52:03 +0200

    Initial commit

---
+++ app/src/main/java/edu/rit/pokedexfinalproject/MainActivity.kt
@@ -0,0 +1,14 @@
+package edu.rit.pokedexfinalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.io.IOException
+import java.io.InputStream
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/edu/rit/pokedexfinalproject/Pokemon.kt
@@ -0,0 +1,10 @@
+package edu.rit.pokedexfinalproject
+
+data class Pokemon (
+        val name: String,
+        val id: String,
+        val location: String,
+        val img: String,
+        val description: String,
+        val type: String
+        )
